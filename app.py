247c835eb284cb989a8d02b8d45efb4d9f57a1d4
Mike_Jackson <mikejackson35@gmail.com>
2024-03-11 15:21:12 -0700

    update

--- app.py
+++ app.py
@@ -17,12 +17,10 @@ config = {'displayModeBar': False}
 
 
 dg_key = st.secrets.dg_key
-# dg = f"https://feeds.datagolf.com/preds/fantasy-projection-defaults?tour=pga&site=draftkings&slate=main&format=csv&key={dg_key}"
 
 st.cache_data()
 def get_projections():
     dg_proj = pd.read_csv(f"https://feeds.datagolf.com/preds/fantasy-projection-defaults?tour=pga&site=draftkings&slate=main&file_format=csv&key={dg_key}",usecols=['player_name','proj_points_total'])
-    dg_proj.columns = ['player','proj_pts']
     return dg_proj
 dg_proj = get_projections()
 dg_proj_copy = dg_proj.copy()
@@ -38,7 +36,7 @@ teams_dict = {'919':'Philly919','u_c':'unit_circle','NT 4':'New Team 4','NT 8':'
 teams['team'] = teams.team.map(teams_dict)
 teams.set_index('player',inplace=True)
 
-# dg_proj.columns = ['player','proj_pts']
+dg_proj.columns = ['player','proj_pts']
 
 names = dg_proj['player'].str.split(expand=True)
 names[0] = names[0].str.rstrip(",")
