ae55deeb52579e5986cb8c4b178d8cc2d5b08ea0
Mike_Jackson <mikejackson35@gmail.com>
2024-04-11 22:34:10 -0700

    updtae

--- pages/live.py
+++ pages/live.py
@@ -45,8 +45,22 @@ teams = teams.loc[teams.active_reserve=='Active'].set_index('player')
 ## MERGE & PROCESS ##
 
 # merge current fantasy teams and live scoring
-live_merged = pd.merge(teams, live, how='left', left_index=True, right_index=True)[['team','team_short','position','total','round','thru','sg_putt','sg_arg','sg_app','sg_ott','sg_t2g']].fillna(0).sort_values('total')
-live_merged = live_merged.convert_dtypes().reset_index()
+live_merged = pd.merge(teams, live,
+                       how='left', left_index=True, right_index=True)[
+                           ['team',
+                            'team_short',
+                            'position',
+                            'total',
+                            'round',
+                            'thru',
+                            'sg_putt',
+                            'sg_arg',
+                            'sg_app',
+                            'sg_ott',
+                            'sg_t2g']
+                            ].fillna(0).sort_values('total').convert_dtypes().reset_index()
+
+# live_merged = live_merged.convert_dtypes().reset_index()
 # add columns matchup_num & holes_remaining
 live_merged['matchup_num'] = live_merged.team.map(matchups)
 live_merged['holes_remaining'] = (72 - (live_merged['thru']).fillna(0)).astype(int)
