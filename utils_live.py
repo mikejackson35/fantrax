750a973e81da170517471e9d5fe66cd6fefa4346
Mike_Jackson <mikejackson35@gmail.com>
2024-05-01 13:07:02 -0700

    update

--- utils_live.py
+++ utils_live.py
@@ -36,7 +36,7 @@ def highlight_rows(row):
     elif value == 'AlphaWired':
         color = '#ff0000' # Orange
     elif value == "Snead's Foot":
-        color = '#ABEBC6' # Green
+        color = '#82E0AA' # Green
     elif value == 'New Team 4':
         color = '#7f8c9b' # Red
     elif value == 'Team Gamble':
@@ -76,7 +76,7 @@ def highlight_rows_team_short(row):
     elif value == '[AW]':
         color = '#ff0000' # Orange
     elif value == 'NT 8':
-        color = '#ABEBC6' # Green
+        color = '#82E0AA' # Green
     elif value == 'NT 4':
         color = '#7f8c9b' # Red
     elif value == 'MG':
