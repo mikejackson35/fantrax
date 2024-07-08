aaa21f70743ba515d9f1b1daf7a11c5874fac3c2
Mike_Jackson <mikejackson35@gmail.com>
2024-07-08 09:35:38 -0700

    update

--- constants.py
+++ constants.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 dg_key = st.secrets.dg_key
 
-TOURNAMENT_NAME = "John Deere Classic"
-WEEK_NUMBER = 25
+TOURNAMENT_NAME = "The Scottish Open"
+WEEK_NUMBER = 26
 CUTLINE = 66
 
 LIVE_STATS = f"https://feeds.datagolf.com/preds/live-tournament-stats?stats=sg_putt,sg_arg,sg_app,sg_ott,sg_t2g,sg_bs,sg_total,distance,accuracy,gir,prox_fw,prox_rgh,scrambling&round=event_avg&display=value&file_format=csv&key={dg_key}"
@@ -11,10 +11,12 @@ FANTASY_PROJECTIONS = f"https://feeds.datagolf.com/preds/fantasy-projection-defa
 
 tickvals = [1,2,3,4,5,6,7,8,9,10, \
             11,12,13,14,15,16,17,18,19,20, \
-            21,22,23,24]
+            21,22,23,24,25]
 
-ticktext = ['Sony','Amex','Farmers','AT&T','Waste Mgmt','Genesis','Mexico Open','Cognizant','Arnold Palmer','PLAYERS', \
-            'Valspar','Houston Open','Valero','The Masters','RBC Heritage','AT&T Byron Nelson','Wells Fargo','PGA Championship','Charles Schwab', 'RBC Canadian', \
-            'The Memorial','US Open','Travelers','Rocket Mortgage']
+ticktext = ['Sony','Amex','Farmers','AT&T','Waste Mgmt','Genesis','Mexico Open', \
+            'Cognizant','Arnold Palmer','PLAYERS', \
+            'Valspar','Houston Open','Valero','The Masters','RBC Heritage','AT&T Byron Nelson',\
+            'Wells Fargo','PGA Championship','Charles Schwab', 'RBC Canadian', \
+            'The Memorial','US Open','Travelers','Rocket Mortgage','John Deere Classic']
 
 no_cut_weeks = [4,15,17,21,23]
